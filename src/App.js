ed0afe2eca53a28f65b6a8bb873b0840b5cfb90a
jingli <jingli.claire@gmail.com>
2019-07-08 16:24:06 +1000

    install dependencies dotenv, axios, react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+// import line that for future use
+
+// require("dotenv").config();
+// import axios from "axios";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link,
+//   Switch,
+//   Redirect
+// } from "react-router-dom";
 
 function App() {
   return (
