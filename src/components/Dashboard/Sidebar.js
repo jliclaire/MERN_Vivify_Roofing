b0f7ecfc7a314a1461d6c72e3d177866306c080f
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-07-11 13:25:32 +1000

    Add styling and HTML structure for sidebar

--- src/components/Dashboard/Sidebar.js
+++ src/components/Dashboard/Sidebar.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import './sidebar.css'
 
 const Sidebar = (props) => {
   const { data } = props;
   return (
-    <>
-      <h1>Hello world</h1>
-      <p>There are {data.length} leads to be sold.</p>
-    </>
+    <div className='sidebar'>
+      <h2 className='sidebar-logo'>VIVIFY</h2>
+      <div className='sidebar-user'>
+        <div>portrait</div>
+        <div>name</div>
+      </div>
+      <div className='sidebar-leadboxes'>
+        <div>Inbox ({data.length})</div>
+        <div>Followup</div>
+        <div>Archive</div>
+      </div>
+      <div className='sidebar-bottom'>
+        <div>Admin</div>
+        <div>Logout</div>
+      </div>
+    </div>
   )
 }
 
