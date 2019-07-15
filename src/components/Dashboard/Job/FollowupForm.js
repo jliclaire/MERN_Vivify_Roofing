b7648c1a78d0aea7c6993db8378c000b5402db9a
jingli <jingli.claire@gmail.com>
2019-07-15 12:52:24 +1000

    created followup form, added css to it

--- src/components/Dashboard/Job/FollowupForm.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class FollowupForm extends Component {
-  render() {
-    return <div className="job-followup-form">FollowupForm</div>;
-  }
-}
-
-export default FollowupForm;
