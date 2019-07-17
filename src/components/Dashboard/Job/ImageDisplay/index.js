a7e5ef508ead26801f475e71e6fc3d8823d5bea7
Bull-Butcher <markjamesbutcher@gmail.com>
2019-07-17 16:26:22 +1000

    image popup display now working

--- src/components/Dashboard/Job/ImageDisplay/index.js
+++ src/components/Dashboard/Job/ImageDisplay/index.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import './imageDisplay.css'
+import PhotoEnlarge from './PhotoEnlarge'
+
 
 
 class ImageDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeImage: 0,
+      showPopup: false,
+    };
+  }
 
-  
+  toggleImagePopup = (e) => {
+    this.setState({
+      activeImage: parseInt(e.target.id),
+      showPopup: !this.state.showPopup,
+    })
+  }
+
+
+
   render() {
     const {data} = this.props;
     const links = data.imageUrls
-    const link = links.map(x => <img className="photo-frame" src={x} alt="Logo" width="90" height="110"/>)
+    
     return (
-      <>
-        <div className="photos-wrapper" >{link}</div>
-      </>
+      <div className='photos-wrapper'>
+        {
+          links.map((url, i) => {
+            return <img className="photo-frame" id={i} src={url} alt="" width="90" height="110" onClick={this.toggleImagePopup.bind(this)}/>
+          })
+        }
+        {
+          this.state.showPopup &&
+          <PhotoEnlarge 
+            data={links[this.state.activeImage]}
+            close={this.toggleImagePopup.bind(this)}
+          />
+        }
+      </div>
     )
   }
 }
