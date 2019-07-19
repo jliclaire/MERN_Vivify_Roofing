0dfcfbfa0574689c820409485d63eefdb9b28142
Andrew Sims <acsims@tuta.io>
2019-07-20 09:46:38 +1000

    Acknowledgement of painting quote is conditionally rendered

--- src/components/Dashboard/Job/Enquiry/CustomerComment.js
+++ src/components/Dashboard/Job/Enquiry/CustomerComment.js
@@ -6,9 +6,15 @@ class CustomerComment extends Component {
   render() {
     const { data } = this.props;
     return (
-      <p className="job-enquiry-customer-comment margin-top-bottom p-font">
-        <span className="comments">Comments:</span> {data.comments}
-      </p>
+      <>
+        <p className="job-enquiry-customer-comment margin-top-bottom p-font">
+          <span className="comments">Comments:</span> {data.comments}
+        </p>
+        {
+          data.paintingQuote &&
+          <h3>Note: this is a painting quote</h3>
+        }
+      </>
     );
   }
 }
