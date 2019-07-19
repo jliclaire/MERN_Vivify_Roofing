3389d7e476e3716f784b65e5c8d58b334513a5cb
Andrew Sims <acsims@tuta.io>
2019-07-19 15:36:11 +1000

    Active job is blue on the Jobs list

--- src/components/Dashboard/JobList/index.js
+++ src/components/Dashboard/JobList/index.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 import JobListRow from "./JobListRow";
 
 class JobList extends Component {
-
   render() {
-    const { data, setActiveJob, show } = this.props;
+    const { data, setActiveJob, show, activeId } = this.props;
     if (show) {
-      return <JobListRow data={data} setActiveJob={setActiveJob} />;
+      return <JobListRow data={data} setActiveJob={setActiveJob} activeId={activeId} />;
     } else {
       return null;
     }
