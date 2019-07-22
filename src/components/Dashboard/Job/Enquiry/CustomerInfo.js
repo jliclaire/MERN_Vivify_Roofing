6864368bc1e9156dc8554e46c1a7ebe6a9afdb9a
Andrew Sims <acsims@tuta.io>
2019-07-22 13:24:11 +1000

    Phone number as a link to phone

--- src/components/Dashboard/Job/Enquiry/CustomerInfo.js
+++ src/components/Dashboard/Job/Enquiry/CustomerInfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./customerInfo.css";
-import { capitalise, capitaliseMultiple } from '../../../../utils/capitalise'
+import { capitaliseMultiple } from '../../../../utils/capitalise'
+import { FaPhone } from 'react-icons/fa';
 
 class CustomerInfo extends Component {
 
@@ -11,11 +12,11 @@ class CustomerInfo extends Component {
       <div className="job-enquiry-customer margin-top-bottom">
         <div className="name-suburb">
           <h3>{capitaliseMultiple(data.name)}</h3>
-          <h5>{capitalise(data.suburb)}</h5>
+          <h5>{capitaliseMultiple(data.suburb)}</h5>
         </div>
         <div className="email-phone-date">
           <h5>{data.email}</h5>
-          <h5>Ph# {data.phone}</h5>
+          <a href={'tel:' + data.phone}><h5><FaPhone /> {data.phone}</h5></a>
           <h5>Received {new Date(data.createdTime).toLocaleDateString()}</h5>
         </div>
       </div>
