8cc7ebf7dcc94004ac19af8d86e093c41d62fa5c
jingli <jingli.claire@gmail.com>
2019-07-23 17:02:37 +1000

    mobile css for add new lead form in progress

--- src/components/Dashboard/Job/Enquiry/CustomerInfo.js
+++ src/components/Dashboard/Job/Enquiry/CustomerInfo.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
 import "./customerInfo.css";
-import { capitaliseMultiple } from '../../../../utils/capitalise'
-import { FaPhone, FaEnvelope } from 'react-icons/fa';
+import { capitaliseMultiple } from "../../../../utils/capitalise";
+import { FaPhone, FaEnvelope } from "react-icons/fa";
 
 class CustomerInfo extends Component {
-
   render() {
     const { data } = this.props;
     return (
-      
       <div className="job-enquiry-customer margin-top-bottom">
         <div className="name-suburb">
           <h3>{capitaliseMultiple(data.name)}</h3>
           <h5>{capitaliseMultiple(data.suburb)}</h5>
         </div>
         <div className="email-phone-date">
-          <a href={'mailto:' + data.email}>
-            <h5><FaEnvelope size='2em' className='contact-icon'/> {data.email}</h5>
+          <a href={"mailto:" + data.email}>
+            <h5>
+              <FaEnvelope size="1em" className="contact-icon" /> {data.email}
+            </h5>
           </a>
-          <a href={'tel:' + data.phone}>
-            <h5><FaPhone size='2em' className='contact-icon'/> {data.phone}</h5>
+          <a href={"tel:" + data.phone}>
+            <h5>
+              <FaPhone size="1em" className="contact-icon" /> {data.phone}
+            </h5>
           </a>
           <h5>Received {new Date(data.createdTime).toLocaleDateString()}</h5>
         </div>
