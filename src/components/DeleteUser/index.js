a8cb2a64a7d4e59b854872af1046f58cbd29acba
jingli <jingli.claire@gmail.com>
2019-07-25 10:47:46 +1000

    add mobile css for delete users page

--- src/components/DeleteUser/index.js
+++ src/components/DeleteUser/index.js
@@ -24,7 +24,6 @@ class DeleteUser extends Component {
       await axios.delete(`${process.env.REACT_APP_API_URL}/users/${userId}`, {
         headers: { token: token }
       });
-
       const deleteMessage = `Successfully deleted account for ${
         deletedUser.name
       }!`;
@@ -44,39 +43,72 @@ class DeleteUser extends Component {
     {
       remainingUsers ? (data = remainingUsers) : (data = salesUsers);
     }
-    return (
-      <div className="delete-user-dashboard">
-        <h1>All Sales Account</h1>
-        {this.state.deleteMessage ? <h4>{this.state.deleteMessage}</h4> : null}
-        <div className="salesUser-container">
-          <div className="user-row p-font comments">
-            <div className="user-col">Name</div>
-            <div className="user-col">Role</div>
-            <div className="user-col">Phone</div>
-            <div className="col-width">Email</div>
-            <div className="user-col text-center">Delete Account</div>
+    if (window.innerWidth < 767) {
+      return (
+        <div className="delete-user-dashboard">
+          <h1>All Sales Account</h1>
+          {this.state.deleteMessage ? (
+            <h4>{this.state.deleteMessage}</h4>
+          ) : null}
+          <div className="salesUser-container">
+            <div className="user-row p-font comments">
+              <div className="user-col">Name</div>
+              <div className="user-col text-center">Delete Account</div>
+            </div>
+            {data.map((user, index) => {
+              return (
+                <div key={index} className="user-row p-font" id={user._id}>
+                  <div className="user-col">{user.name}</div>
+                  <i
+                    className="far fa-trash-alt user-col text-center"
+                    onClick={this.handleDeleteUser}
+                  />
+                </div>
+              );
+            })}
           </div>
-          {data.map((user, index) => {
-            return (
-              <div key={index} className="user-row p-font" id={user._id}>
-                <div className="user-col">{user.name}</div>
-                <div className="user-col">{user.role}</div>
-                <div className="user-col">{user.phone}</div>
-                <div className="col-width">{user.email}</div>
-                <i
-                  className="far fa-trash-alt user-col text-center"
-                  onClick={this.handleDeleteUser}
-                />
-              </div>
-            );
-          })}
+          <div className="btn-register back-to-dashboard-btn">
+            <Link to="/"> Back to Dashboard</Link>
+          </div>
         </div>
+      );
+    } else {
+      return (
+        <div className="delete-user-dashboard">
+          <h1>All Sales Account</h1>
+          {this.state.deleteMessage ? (
+            <h4>{this.state.deleteMessage}</h4>
+          ) : null}
+          <div className="salesUser-container">
+            <div className="user-row p-font comments">
+              <div className="user-col">Name</div>
+              <div className="user-col">Role</div>
+              <div className="user-col">Phone</div>
+              <div className="col-width">Email</div>
+              <div className="user-col text-center">Delete Account</div>
+            </div>
+            {data.map((user, index) => {
+              return (
+                <div key={index} className="user-row p-font" id={user._id}>
+                  <div className="user-col">{user.name}</div>
+                  <div className="user-col">{user.role}</div>
+                  <div className="user-col">{user.phone}</div>
+                  <div className="col-width">{user.email}</div>
+                  <i
+                    className="far fa-trash-alt user-col text-center"
+                    onClick={this.handleDeleteUser}
+                  />
+                </div>
+              );
+            })}
+          </div>
 
-        <div className="btn-register back-to-dashboard-btn">
-          <Link to="/"> Back to Dashboard</Link>
+          <div className="btn-register back-to-dashboard-btn">
+            <Link to="/"> Back to Dashboard</Link>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
